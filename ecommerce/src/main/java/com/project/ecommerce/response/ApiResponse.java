beb085b0bf6af93fb8fcb230f5d29001ecd4889f
YTeyfik <teyfikyalav@gmail.com>
2024-09-25 12:54:53 +0300

    implementing the image controller

---
+++ ecommerce/src/main/java/com/project/ecommerce/response/ApiResponse.java
@@ -0,0 +1,11 @@
+package com.project.ecommerce.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse {
+	private String message;
+	private Object data;
+}
